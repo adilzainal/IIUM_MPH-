8e2da83e447d38095bddb84cf2184ca622a142de
Adilzainal <70354345+adilzainal@users.noreply.github.com>
2025-11-11 15:32:35 +0800

    Update week10nonparametric.R

--- week10nonparametric.R
+++ week10nonparametric.R
@@ -91,3 +91,18 @@ install.packages("datarium")
 library(datarium)
 data("selfesteem", package = "datarium")
 head(selfesteem, 3)
+
+selfesteem <- selfesteem %>%
+  gather(key = "time", value = "score", t1, t2, t3) %>%
+  convert_as_factor(id, time)
+head(selfesteem, 3)
+
+selfesteem %>%
+  group_by(time) %>%
+  get_summary_stats(score, type = "common")
+
+ggboxplot(selfesteem, x = "time", y = "score", add = "jitter")
+
+res.fried <- selfesteem %>% friedman_test(score ~ time |id)
+res.fried
+#p<0.05 significant
