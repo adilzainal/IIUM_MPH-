bcffbd21813c00b9614ceb5c76203ccd0c2b0113
Adilzainal <70354345+adilzainal@users.noreply.github.com>
2025-11-11 15:29:16 +0800

    Add files via upload

---
+++ week10nonparametric.R
@@ -0,0 +1,93 @@
+#--------------------------------------------------------
+# Biostatistic practical using R Software (2025) Week 10 MPH Non parametric
+#--------------------------------------------------------
+# Muhammad Adil ZA
+#--------------------------------------------------------
+# Bivariate data analysis continued
+#--------------------------------------------------------
+#Non parametric test (mann whitney, kruskall wallis, wilcoxon, spearman, friedman)
+
+
+#mann whitney u test (non parametric for independent t-test)
+library(psych)
+library(purrr)
+library(dplyr)
+data.sav %>% 
+  group_by(data.sav$sex) %>% 
+  summary(data.sav$wt)
+describeBy(data.sav$wt, data.sav$sex)
+stat.desc(wt, sex)
+attach(data.sav)
+wilcox.test(wt ~ sex, data=data.sav) 
+
+describe.by(data.sav$wt2, data.sav$sex)
+wilcox.test(wt2 ~ sex, data=data.sav)
+
+ggboxplot(data.sav, x = "sex", y = "wt2", 
+          color = "sex", palette = c("#FF3300", "#E7B800"),
+          order = c("Female", "Male"),
+          ylab = "Weight after intervention (kg)", xlab = "Sex")
+
+#kruskall wallis (non parametric for anova)
+describeBy(data.sav$wt, data.sav$exercise)
+kruskal.test(wt ~ exercise, data = data.sav)
+attach(data.sav)
+describe.by(data.sav$wt, data.sav$exercise)
+kruskal.test(wt ~ exercise, data = data.sav)
+pairwise.wilcox.test(data.sav$wt, data.sav$exercise,
+                     p.adjust.method = "BH")   #post hoc test if the p value is significant
+
+library("ggpubr")
+ggboxplot(data.sav, x = "exercise", y = "wt", 
+          color = "exercise", palette = c("#FF3300", "#E7B800", "#00FFFF"),
+          order = c("Low", "Moderate", "High"),
+          ylab = "Weight", xlab = "Exercise")
+
+describe.by(data.sav$sbp, data.sav$exercise)
+kruskal.test(sbp ~ exercise, data = data.sav)
+pairwise.wilcox.test(data.sav$sbp, data.sav$exercise,
+                     p.adjust.method = "BH")   #post hoc test if the p value is significant
+
+library("ggpubr")
+ggboxplot(data.sav, x = "exercise", y = "sbp", 
+          color = "exercise", palette = c("#FF3300", "#E7B800", "#00FFFF"),
+          order = c("Low", "Moderate", "High"),
+          ylab = "Systolic", xlab = "Exercise")
+
+#wilcoxon signed rank test (non parametric for pair t -test)
+summary(data.sav$BMI)
+summary(data.sav$BMI2)
+wilcox.test(data.sav$BMI, data.sav$BMI2, paired=TRUE) 
+
+#correlation 
+plot(data.sav$wt, data.sav$hcy, main="Scatterplot Example", 
+     xlab="Weight ", ylab="Hcy ", pch=20)
+
+plot(data.sav$sbp, data.sav$dbp, main="Scatterplot new", 
+     xlab="Systolic ", ylab="Diastolic ", pch=20)
+
+# Simple Scatterplot
+plot(data.sav$sbp, data.sav$dbp, main="Scatterplot Example", 
+     xlab="SBP ", ylab="DBP ", pch=19)
+
+# Add fit lines
+abline(lm(data.sav$hcy~data.sav$wt), col="red") # regression line (y~x) 
+lines(lowess(data.sav$wt,data.sav$ht), col="blue") # lowess line (x,y)
+
+
+library(Hmisc)
+cor.test(data.sav$hcy, data.sav$wt, method=c("pearson")) #normally distributed data
+cor.test(data.sav$hcy, data.sav$wt, method=c("spearman")) #non-normally distributed data
+cor.test(data.sav$sbp, data.sav$dbp, method=c("pearson")) #normally distributed data
+
+#r <0.3 very weak, 0.3-0.5< is weak, 0.5-0.7 is moderate, >0.7 is strong
+#interpret correlation coefficent = 1.-value 2.direction 3.p-value
+
+#friedman test 
+library(tidyverse)
+library(ggpubr)
+library(rstatix)
+install.packages("datarium")
+library(datarium)
+data("selfesteem", package = "datarium")
+head(selfesteem, 3)
